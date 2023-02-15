0fea0f42dcb229ccc1aa60ae5bd2bcd7f7a98868
shakira-git <shakirashafi07@gmail.com>
2023-02-15 16:01:38 +0530

    log added

--- src/main/java/testjen/Testing.java
+++ src/main/java/testjen/Testing.java
@@ -1,10 +1,14 @@
 package testjen;
 
+import java.util.logging.Logger;
+
 public class Testing {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("SUCESS!!!   " +System.currentTimeMillis());
+		Logger log = Logger.getLogger("log");
+		log.info("success");
 	}
 
 }
